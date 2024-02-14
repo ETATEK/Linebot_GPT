e117fe7bf37131d8b38571eda88fa93251d6f1f4
CAExpert <python.eta@gmail.com>
2024-02-14 09:33:12 +0800

    v1.2

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
 def GPT_response(text):
-    # 接收回應
-    response = openai.Completion.create(model="gpt-3.5-turbo-instruct", prompt=text, temperature=0.5, max_tokens=500)
+    # 接收回應, 下行要確認
+    response = openai.Completion.create(model="gpt-3.5-turbo", prompt=text, temperature=0.5, max_tokens=500)
     print(response)
     # 重組回應
     answer = response['choices'][0]['text'].replace('。','')
